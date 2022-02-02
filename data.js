ae718362349a565231deb9e8daf5eecb2965f2a0
alina1030 <85539852+alina1030@users.noreply.github.com>
2022-02-02 13:55:46 +0530

    "upload done"

--- data.js
+++ data.js
@@ -1,1 +1,10 @@
+const data=(name,year,qualification)=>{
+    userdata={
+        name:name,
+        Year:year,
+        Qualification:qualification
+    }
+    return userdata
+}
 
+module.exports = data
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+
+
+const data = require('./data')
+const os = require('os')
+const fs = require('fs')
+const crypto =require('crypto')
+const secret='Hello';
+const value = os.userInfo()
+fs.writeFileSync("message.txt", "message.txt \n")
+const userdata= data("Prograd",2020,"BE")
+const password = crypto.createHmac('sha256', secret).update('Prograd').digest('hex');
+fs.appendFileSync("message.txt", "ProGrad Details: \n")
+fs.appendFileSync("message.txt", "\tUserId ->         101, \n")
+fs.appendFileSync("message.txt", `\tName ->           ${userdata.name},\n`)
+fs.appendFileSync("message.txt", `\tYear ->           ${userdata.Year},\n`)
+fs.appendFileSync("message.txt", `\tQualification ->  ${userdata.Qualification},\n`)
+fs.appendFileSync("message.txt", `\tUserName ->       ${value.username},\n`)
+fs.appendFileSync("message.txt", `\tPassword ->       ${password}\n`)
